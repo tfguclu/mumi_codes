b40f92493e891bbceac8dc5f6f0fdd8123f02dff
tfguclu <tfguclu@gmail.com>
2016-01-13 21:15:01 +0200

    mistake corrected

--- config_file_jobgen.py
+++ config_file_jobgen.py
@@ -9,15 +9,15 @@ import os
 
 f = open("config_joblist", 'w')
 
-f.write("#!/bin/bash")
-f.write("echo config jobs started")
+f.write("#!/bin/bash\n")
+f.write("echo config jobs started\n")
 
 for file in os.listdir('.'):
     if fnmatch.fnmatch(file, '*_config.conf'):
         config = file
         file_name_wh_ex = str(os.path.splitext(config)[0])
         logfile = str(file_name_wh_ex+".log")
-        f.write("namd2 +p8 %s > %s" % (str(config), str(logfile)))
-        f.write("echo job for %s is finished" % str(config))
+        f.write("namd2 +p8 %s > %s\n" % (str(config), str(logfile)))
+        f.write("echo job for %s is finished\n" % str(config))
 
 f.close()
